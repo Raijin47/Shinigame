a8188f83ad911e93b1c4bc2866dc544cf8e24118
Raijin47 <tony_crosh@mail.ru>
2023-08-16 16:36:52 +0300

    Add sprite sheet animation, delete DragonBones

--- Assets/Scripts/Enemy/EnemyFade.cs
+++ Assets/Scripts/Enemy/EnemyFade.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using UnityEngine;
-using DragonBones;
 
 public class EnemyFade : MonoBehaviour
 {
     [SerializeField] private float _speedFade;
 
-    MeshRenderer _renderer;
+    SpriteRenderer _renderer;
     MaterialPropertyBlock _block;
     MaterialPropertyBlock _blockBurning;
     Enemy _enemy;
-    UnityArmatureComponent _component;
 
     float _burnValue;
     float _fadeValue;
@@ -18,18 +16,14 @@ public class EnemyFade : MonoBehaviour
     int fadePropertyID;
     int firePropertyID;
 
-    readonly string Run = "run";
 
     void Start()
     {
         _enemy = GetComponentInParent<Enemy>();
         _fadeValue = 1;
-        _renderer = GetComponentInChildren<MeshRenderer>();
+        _renderer = GetComponent<SpriteRenderer>();
         _block = new MaterialPropertyBlock();
         _blockBurning = new MaterialPropertyBlock();
-        _component = GetComponent<UnityArmatureComponent>();
-
-        _component.animation.Play(Run);
 
         fadePropertyID = Shader.PropertyToID("_FullGlowDissolveFade");
         firePropertyID = Shader.PropertyToID("_BurnFade");
